08a96e59fdc05b702bcaf6aa93f740eb86963804
Murilex01 <murilo.sxv@gmail.com>
2024-10-07 20:34:31 -0300

    primeiro commit

---
+++ Aula09/auth-api/app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const express = require('express')
+const mongoose = require('mongoose');
+const app = express();
+const authRoutes = require('./routes/authRoutes');
+
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+
+mongoose.connect(process.env,MONGO_URI,{
+    useNewUrlparser: true,
+    useUnifiedTopology: true
+})
+
+.then (() => console.log ('conectado ao MongoDB'))
+.catch((error) => console.error ('Erro ao conectar ao MongoDB', error));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+ 
---
+++ Aula09/auth-api/controlles/authController.js
@@ -0,0 +1,36 @@
+const User = require('../models/user');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+    const {username, password } = req.body;
+
+    try{
+        const hashedPassword = await bcrypt.hash(password, 10);
+        
+        const user = newUser = new User ({ username, password: hashedPassword});
+        await newUser.save();
+        res.status(201).json({ message: 'Usuário registrado com sucesso' });
+        } catch (error) {
+            res.status(400).json({ message: 'Erro ao registrar usuário' });
+    }    
+};
+
+exports.login = async (req, res) => {
+    const { username, password } = req.body;
+
+    try{
+        const user = await User.findOne({ username });
+        if (!user) return res.status(401).json({ message: 'Usuário não encontrado'});
+        
+        const is = await bcrypt.compare(password, user.password);
+        if (!is) return res.status(401).json({ message: 'Senha inválida'});
+
+
+        const token = jwt.sign({ id: user._id}, process.env.JWT_SECRET, { expiresIn: '1h'});
+
+        res.json({ token });
+    }catch (error) {
+        res.status(400).json({ message: 'Erro ao realizar login' });
+    }
+    };
